c103fb4a919170931c24d2758a72b0f1bc432e02
mbougar <146754886+mbougar@users.noreply.github.com>
2023-12-01 22:20:07 +0100

    Final update

--- test/test_agenda.py
+++ test/test_agenda.py
@@ -10,12 +10,18 @@ from src.agenda import(
     validar_telefono,
     buscar_contacto,
     pedir_opcion,
+    pedir_nombre_y_apellido,
+    separar_nombre_y_apellido,
+    vaciar_agenda,
+    pedir_opcion_limitado,
+    crear_lista_telefonos_formato,
+    buscar_contacto_por_criterio
 )
 
 
 # Simulamos un archivo CSV con datos de contacto para las pruebas
 RUTA = pathlib.Path(__file__).parent.absolute() 
-NOMBRE_FICHERO = 'contactos.csv'
+NOMBRE_FICHERO = "contactos.csv"
 RUTA_FICHERO = path.join(RUTA, NOMBRE_FICHERO)
 CONTACTOS_PRUEBA = [
     {"nombre": "Laura", "apellido": "Iglesias", "email": "liglesias@gmail.com", "telefonos": ["666777333", "666888555", "607889988"]},
@@ -48,13 +54,13 @@ def test_validar_email(contactos_iniciales):
 
 
 def test_pedir_email(monkeypatch, contactos_iniciales):
-    monkeypatch.setattr('builtins.input', lambda _: "")
+    monkeypatch.setattr("builtins.input", lambda _: "")
     with pytest.raises(ValueError, match="el email no puede ser una cadena vacía"):
         pedir_email(contactos_iniciales, False)
-    monkeypatch.setattr('builtins.input', lambda _: "correosinarroba.com")
+    monkeypatch.setattr("builtins.input", lambda _: "correosinarroba.com")
     with pytest.raises(ValueError, match="el email no es un correo válido"):
         pedir_email(contactos_iniciales, False)
-    monkeypatch.setattr('builtins.input', lambda _: "marcopete@gmail.com")
+    monkeypatch.setattr("builtins.input", lambda _: "marcopete@gmail.com")
     with pytest.raises(ValueError, match="el email ya existe en la agenda"):
         pedir_email(contactos_iniciales, True)
 
@@ -90,9 +96,51 @@ def test_buscar_contacto_params(contactos_iniciales, input_email, expected):
 
 
 def test_pedir_opcion(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "3")
+    monkeypatch.setattr("builtins.input", lambda _: "3")
     resultado = pedir_opcion()
     assert resultado == 3
-    monkeypatch.setattr('builtins.input', lambda _: "a")
+    monkeypatch.setattr("builtins.input", lambda _: "a")
     resultado = pedir_opcion()
     assert resultado == -1
+
+
+#Principio de las funciones creadas por mi
+def test_pedir_nombre_y_apellido(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "Manuel Carlos Bouza")
+    resultado = pedir_nombre_y_apellido()
+    assert resultado == ("Manuel Carlos", "Bouza")
+
+
+def test_separar_nombre_y_apellido():
+    resultado = separar_nombre_y_apellido(["Manuel", "Carlos", "Bouza"])
+    assert resultado == ("Manuel Carlos", "Bouza")
+
+
+def test_vaciar_agenda():
+    contactos = CONTACTOS_PRUEBA
+    resultado = vaciar_agenda(contactos)
+    assert resultado == []
+
+
+def test_pedir_opcion_limitado(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "3")
+    resultado = pedir_opcion_limitado()
+    assert resultado == 3
+    monkeypatch.setattr("builtins.input", lambda _: "a")
+    resultado = pedir_opcion_limitado()
+    assert resultado == -1
+
+
+def test_crear_lista_telefonos_formato():
+    lista_telefonos_original = ["+34123456789", "+34987654321", "123123123"]
+    resultado = crear_lista_telefonos_formato(lista_telefonos_original)
+    assert resultado == ["+34-123456789", "+34-987654321", "123123123"]
+
+
+def test_buscar_contacto_por_criterio():
+    contactos = CONTACTOS_PRUEBA
+    resultado = buscar_contacto_por_criterio(contactos, "apellido", "Amargo")
+    assert resultado == [1]
+    contactos = CONTACTOS_PRUEBA
+    resultado = buscar_contacto_por_criterio(contactos, "telefonos", "610000099")
+    assert resultado == [5]
